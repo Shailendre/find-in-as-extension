8828f565e3b034ccdc5bddd8f392ca22a9c91615
ssingh <ssingh@alpha-sense.com>
2021-03-03 22:36:04 +0530

    initial commit

---
+++ script/as-context-menu.js
@@ -0,0 +1,23 @@
+// context menu id
+const CONTEXT_MENU_ID = "search-in-as-context-menu";
+
+// create a context-menu
+chrome.contextMenus.create({
+    id: CONTEXT_MENU_ID,
+    title: "Search in AlphaSense",
+    contexts: ["selection"],
+});
+
+// add listener for context menu
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+
+    if (info.menuItemId === CONTEXT_MENU_ID) {
+
+        let phrase = info.selectionText;
+        console.log("selected phrase " + phrase);
+
+        chrome.tabs.create({
+            'url': 'https://research.alpha-sense.com/'
+        }, function (tab){});
+    }
+});
